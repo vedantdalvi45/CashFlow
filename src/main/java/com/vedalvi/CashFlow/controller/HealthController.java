f76681881f31746a569f303cf981e74f99aad009
vedantdalvi45 <vedalvi16@gmail.com>
2025-09-25 23:01:19 +0530

    Add : HealthController

---
+++ src/main/java/com/vedalvi/CashFlow/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.vedalvi.CashFlow.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Server is running";
+    }
+}
